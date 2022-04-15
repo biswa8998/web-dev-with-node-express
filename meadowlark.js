1156c54c847e4898a13f1315d2ba428a2b9675ae
Biswadeep Dey <biswa8998@gmail.com>
2022-04-15 23:49:00 +0530

    Moved fortune cookie to a module

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,5 @@
 var express = require("express");
-
+var fortune = require("./lib/fortune-cookie.js");
 var app = express();
 
 // setup handlebars view engine
@@ -18,17 +18,8 @@ app.get("/", function (req, res) {
   res.render("home");
 });
 
-var fortunes = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple.",
-];
-
 app.get("/about", function (req, res) {
-  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render("about", { fortune: randomFortune });
+  res.render("about", { fortune: fortune.fortuneCookie() });
 });
 
 // custom 404 page
